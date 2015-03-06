529cc61703bebbe1115e20fad0b9ee9c418e533a
David Blom <d.s.blom@tudelft.nl>
2015-03-06 17:25:15 +0100

    formatting test code

--- src/tests/app/test_sdc.C
+++ src/tests/app/test_sdc.C
@@ -8,24 +8,32 @@
 #include "gtest/gtest.h"
 
 /*
- Implements the piston problem for which a exact solution exists.
- With the exact solution, the convergence orders can be confirmed
- exactly for this test case.
-
- Reference: A. H. van Zuijlen, Efficient Higher Order Time
- Integration of Partitioned Systems, PhD thesis, page 22.
-
- Notes Gaussian quadrature:
- http://undergraduate.csse.uwa.edu.au/units/GENG2140/Mech2012/Gaussian_Quadrature.pdf
- http://en.wikipedia.org/wiki/Gaussian_quadrature
- http://en.wikipedia.org/wiki/Legendre_polynomials
-*/
+ * Implements the piston problem for which a exact solution exists.
+ * With the exact solution, the convergence orders can be confirmed
+ * exactly for this test case.
+ *
+ * Reference: A. H. van Zuijlen, Efficient Higher Order Time
+ * Integration of Partitioned Systems, PhD thesis, page 22.
+ *
+ * Notes Gaussian quadrature:
+ * http://undergraduate.csse.uwa.edu.au/units/GENG2140/Mech2012/Gaussian_Quadrature.pdf
+ * http://en.wikipedia.org/wiki/Gaussian_quadrature
+ * http://en.wikipedia.org/wiki/Legendre_polynomials
+ */
 
 class Piston
 {
 public:
 
-  Piston( int nbTimeSteps, double dt, double q0, double qdot0, double As, double Ac, double omega );
+  Piston(
+    int nbTimeSteps,
+    double dt,
+    double q0,
+    double qdot0,
+    double As,
+    double Ac,
+    double omega
+    );
 
   ~Piston();
 
@@ -39,9 +47,14 @@ public:
     const Eigen::VectorXd & qold,
     const Eigen::VectorXd rhs,
     Eigen::VectorXd & f,
-    Eigen::VectorXd & result );
+    Eigen::VectorXd & result
+    );
 
-  void evaluate( const Eigen::VectorXd & q, const double t, Eigen::VectorXd & f );
+  void evaluate(
+    const Eigen::VectorXd & q,
+    const double t,
+    Eigen::VectorXd & f
+    );
 
   int nbTimeSteps;
   double dt;
@@ -52,70 +65,81 @@ public:
   double Ac;
   double omega;
   int N;
-
 };
 
-Piston::Piston( int nbTimeSteps, double dt, double q0, double qdot0, double As, double Ac, double omega )
-:
-nbTimeSteps( nbTimeSteps ),
-dt( dt ),
-q0( q0 ),
-c1( q0 + Ac / std::pow(omega,2)),
-c2( qdot0 + As / omega),
-As( As ),
-Ac( Ac ),
-omega( omega ),
-N( 2 )
+Piston::Piston(
+  int nbTimeSteps,
+  double dt,
+  double q0,
+  double qdot0,
+  double As,
+  double Ac,
+  double omega
+  )
+  :
+  nbTimeSteps( nbTimeSteps ),
+  dt( dt ),
+  q0( q0 ),
+  c1( q0 + Ac / std::pow( omega, 2 ) ),
+  c2( qdot0 + As / omega ),
+  As( As ),
+  Ac( Ac ),
+  omega( omega ),
+  N( 2 )
 {
   assert( nbTimeSteps > 0 );
   assert( dt > 0 );
 }
 
 Piston::~Piston()
-{
-
-}
+{}
 
 double Piston::referenceSolution( double t )
 {
   double result = c1;
   result += c2 * t;
-  result += - As / std::pow( omega, 2 ) * std::sin( omega * t );
-  result += - Ac / std::pow( omega, 2 ) * std::cos( omega * t );
+  result += -As / std::pow( omega, 2 ) * std::sin( omega * t );
+  result += -Ac / std::pow( omega, 2 ) * std::cos( omega * t );
 
   return result;
 }
 
-void Piston::evaluate( const Eigen::VectorXd & q, const double t, Eigen::VectorXd & f )
+void Piston::evaluate(
+  const Eigen::VectorXd & q,
+  const double t,
+  Eigen::VectorXd & f
+  )
 {
   assert( f.rows() == 2 );
 
-  f(0) = As * std::sin( omega * t );
-  f(0) += Ac * std::cos( omega * t );
-  f(1) = q(0);
+  f( 0 ) = As * std::sin( omega * t );
+  f( 0 ) += Ac * std::cos( omega * t );
+  f( 1 ) = q( 0 );
 }
 
-void Piston::solveTimeStep( const double t,
-const double dt,
-const Eigen::VectorXd & qold,
-const Eigen::VectorXd rhs,
-Eigen::VectorXd & f,
-Eigen::VectorXd & result )
+void Piston::solveTimeStep(
+  const double t,
+  const double dt,
+  const Eigen::VectorXd & qold,
+  const Eigen::VectorXd rhs,
+  Eigen::VectorXd & f,
+  Eigen::VectorXd & result
+  )
 {
   assert( f.rows() == 2 );
   assert( rhs.rows() == 2 );
   assert( result.rows() == 2 );
 
-  f(0) = As * std::sin( omega * t );
-  f(0) += Ac * std::cos( omega * t );
+  f( 0 ) = As * std::sin( omega * t );
+  f( 0 ) += Ac * std::cos( omega * t );
 
   // qdot
-  result(0) = qold(0) + dt * f(0) + rhs(0);
+  result( 0 ) = qold( 0 ) + dt * f( 0 ) + rhs( 0 );
 
   // q
-  result(1) = qold(1) + std::pow(dt,2) * f(0) + dt * qold(0) + dt * rhs(0) + rhs(1);
+  result( 1 ) = qold( 1 ) + std::pow( dt, 2 ) * f( 0 ) + dt * qold( 0 ) + dt * rhs( 0 ) + rhs( 1 );
 
-  f(1) = result(0);
+  f( 1 ) = result( 0 );
 }
 
 class PistonTest : public::testing::Test
@@ -125,7 +149,7 @@ protected:
   virtual void SetUp()
   {
     int nbTimeSteps;
-    double dt ,q0,  qdot0, As,  Ac,  omega, endTime;
+    double dt, q0, qdot0, As, Ac, omega, endTime;
 
     nbTimeSteps = 1000;
     endTime = 100;
