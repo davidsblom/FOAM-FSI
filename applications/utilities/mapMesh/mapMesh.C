365cfb5a457345321b999e9fcb93f454fe89c924
David Blom <d.s.blom@tudelft.nl>
2015-03-30 18:27:30 +0200

    Formatting mapMesh code

--- applications/utilities/mapMesh/mapMesh.C
+++ applications/utilities/mapMesh/mapMesh.C
@@ -11,58 +11,57 @@ int main(
   int argc,
   char * argv[]
   )
+{
+  std::shared_ptr<argList> args( new argList( argc, argv ) );
+
+  if ( !args->checkRootCase() )
   {
-    std::shared_ptr<argList> args( new argList( argc, argv ) );
-
-    if ( !args->checkRootCase() )
-    {
-      FatalError.exit();
-    }
-
-    std::shared_ptr<Time> runTime( new Time
-      (
-        Time::controlDictName,
-        args->rootPath(),
-        args->caseName()
-      ) );
-
-    std::shared_ptr<foamSolidSolver> solid( new SolidSolver( "solid", args, runTime ) );
-
-    std::shared_ptr<foamFluidSolver> fluid ( new CoupledFluidSolver( Foam::fvMesh::defaultRegion, args, runTime ) );
-
-    /*
-    1. Interpolate displacement from solid interface to fluid interface
-    2. Mesh deformation fluid mesh
-    */
-
-    // Step 1: Interpolate displacement from solid interface to fluid interface
-
-    std::shared_ptr<TPSFunction> tpsFunction( new TPSFunction() );
-    rbf::RBFInterpolation rbfSolidToFluidInterface ( tpsFunction );
-
-    Eigen::MatrixXd positions, positionsInterpolation, values, valuesInterpolation;
-
-    solid->getWritePositions( positions );
-    fluid->getReadPositions( positionsInterpolation );
-
-    rbfSolidToFluidInterface.compute(positions, positionsInterpolation);
-
-    // valuesInterpolation = displacement of fluid interface
-    // values = displacement of solid interface
-
-    solid->getDisplacementLocal( values );
-
-    rbfSolidToFluidInterface.interpolate(values, valuesInterpolation);
-
-    std::cout << "step 1 done!" << std::endl;
-
-    // Step 2
-    // Fluid mesh deformation
-
-    fluid->setDisplacementLocal( valuesInterpolation );
-    fluid->moveMesh( fluid->movingPatchesDispl - fluid->movingPatchesDisplOld );
-
-    runTime->loop();
-    runTime->write();
-
+    FatalError.exit();
   }
+
+  std::shared_ptr<Time> runTime( new Time
+    (
+      Time::controlDictName,
+      args->rootPath(),
+      args->caseName()
+    ) );
+
+  assert( Pstream::nProcs() == 1 );
+
+  std::shared_ptr<foamSolidSolver> solid( new SolidSolver( "solid", args, runTime ) );
+
+  std::shared_ptr<foamFluidSolver> fluid( new CoupledFluidSolver( Foam::fvMesh::defaultRegion, args, runTime ) );
+
+  /*
+   * 1. Interpolate displacement from solid interface to fluid interface
+   * 2. Mesh deformation fluid mesh
+   */
+
+  // Step 1: Interpolate displacement from solid interface to fluid interface
+
+  std::shared_ptr<TPSFunction> tpsFunction( new TPSFunction() );
+  rbf::RBFInterpolation rbfSolidToFluidInterface( tpsFunction );
+
+  Eigen::MatrixXd positions, positionsInterpolation, values, valuesInterpolation;
+
+  solid->getWritePositions( positions );
+  fluid->getReadPositions( positionsInterpolation );
+
+  rbfSolidToFluidInterface.compute( positions, positionsInterpolation );
+
+  // valuesInterpolation = displacement of fluid interface
+  // values = displacement of solid interface
+
+  solid->getDisplacementLocal( values );
+
+  rbfSolidToFluidInterface.interpolate( values, valuesInterpolation );
+
+  // Step 2
+  // Fluid mesh deformation
+
+  fluid->setDisplacementLocal( valuesInterpolation );
+  fluid->moveMesh( fluid->movingPatchesDispl - fluid->movingPatchesDisplOld );
+
+  runTime->loop();
+  runTime->write();
+}
