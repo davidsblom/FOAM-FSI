214c32fd1844396ca91801419baacb81203932e2
David Blom <d.s.blom@tudelft.nl>
2015-04-09 09:29:04 +0200

    Modify mapMesh utility to not include the initial displacement when deforming the fluid mesh. The initial displacement of the solid is only needed during a FSI calculation

--- applications/utilities/mapMesh/mapMesh.C
+++ applications/utilities/mapMesh/mapMesh.C
@@ -7,6 +7,70 @@
 #include "RBFInterpolation.H"
 #include "TPSFunction.H"
 
+class MapMeshSolidSolver : public SolidSolver
+{
+public:
+
+  MapMeshSolidSolver(
+    string name,
+    std::shared_ptr<argList> args,
+    std::shared_ptr<Time> runTime
+    )
+    :
+    SolidSolver( name, args, runTime )
+  {}
+
+  void getDisplacementLocal( matrix & displacement )
+  {
+    displacement.resize( getInterfaceSizeLocal(), mesh.nGeometricD() );
+
+    int offset = 0;
+
+    forAll( movingPatchIDs, patchI )
+    {
+      int size = U.boundaryField()[movingPatchIDs[patchI]].size();
+
+      for ( int i = 0; i < size; i++ )
+        for ( int j = 0; j < displacement.cols(); j++ )
+          displacement( i + offset, j ) = U.boundaryField()[movingPatchIDs[patchI]][i][j];
+
+      offset += size;
+    }
+  }
+
+  virtual void getWritePositions( matrix & writePositions )
+  {
+    vectorField writePositionsField( getInterfaceSize(), Foam::vector::zero );
+
+    int globalOffset = 0;
+
+    for ( int i = 0; i < Pstream::myProcNo(); i++ )
+      globalOffset += nGlobalCenters[i];
+
+    int offset = 0;
+
+    forAll( movingPatchIDs, patchI )
+    {
+      const vectorField faceCentres( mesh.boundaryMesh()[movingPatchIDs[patchI]].faceCentres() );
+
+      forAll( faceCentres, i )
+      {
+        writePositionsField[i + offset + globalOffset] = faceCentres[i];
+      }
+
+      offset += faceCentres.size();
+    }
+
+    reduce( writePositionsField, sumOp<vectorField>() );
+
+    writePositions.resize( writePositionsField.size(), mesh.nGeometricD() );
+
+    for ( int i = 0; i < writePositions.rows(); i++ )
+      for ( int j = 0; j < writePositions.cols(); j++ )
+        writePositions( i, j ) = writePositionsField[i][j];
+  }
+};
+
 int main(
   int argc,
   char * argv[]
@@ -28,7 +92,7 @@ int main(
 
   assert( Pstream::nProcs() == 1 );
 
-  std::shared_ptr<foamSolidSolver> solid( new SolidSolver( "solid", args, runTime ) );
+  std::shared_ptr<MapMeshSolidSolver> solid( new MapMeshSolidSolver( "solid", args, runTime ) );
 
   std::shared_ptr<foamFluidSolver> fluid( new CoupledFluidSolver( Foam::fvMesh::defaultRegion, args, runTime ) );
 
