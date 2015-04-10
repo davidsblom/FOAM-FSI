5e56162b8cde5881713bad33e1d6b50001df5a6e
David Blom <d.s.blom@tudelft.nl>
2015-04-10 12:14:20 +0200

    Use the same residual for every flow solver

--- src/fsi/fluidSolvers/CoupledFluidSolver.C
+++ src/fsi/fluidSolvers/CoupledFluidSolver.C
@@ -358,6 +358,11 @@ void CoupledFluidSolver::solve()
 
     scalarField magResU = mag( residual.internalField() );
     scalar momentumResidual = std::sqrt( gSumSqr( magResU ) / mesh.globalData().nTotalCells() );
+    scalar rmsU = std::sqrt( gSumSqr( mag( U.internalField() ) ) / mesh.globalData().nTotalCells() );
+    rmsU /= runTime->deltaT().value();
+
+    // Scale the residual by the root mean square of the velocity field
+    momentumResidual /= rmsU;
 
     int minIter = 2;
     bool convergence = momentumResidual <= convergenceTolerance && oCorr >= minIter - 1;
