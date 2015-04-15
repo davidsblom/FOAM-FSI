0af4a909acbb8bba0cbf04f06c16548debc87cb4
David Blom <d.s.blom@tudelft.nl>
2015-04-15 12:22:29 +0200

    Add utility to perturb the initial velocity field with a random perturbation

---
+++ applications/utilities/addInitialRandomVelocity/addInitialRandomVelocity.C
@@ -0,0 +1,55 @@
+
+#include "fvCFD.H"
+#include "Random.H"
+#include "wallDist.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(
+  int argc,
+  char * argv[]
+  )
+{
+  argList::validArgs.append( "randomPercentage" );
+
+  #   include "setRootCase.H"
+  #   include "createTime.H"
+  #   include "createMesh.H"
+
+  scalar randomPercentage( readScalar( IStringStream( args.additionalArgs()[0] ) () ) );
+
+  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+  Info << "Reading field U\n" << endl;
+  volVectorField U
+  (
+    IOobject
+    (
+      "U",
+      runTime.timeName(),
+      mesh,
+      IOobject::MUST_READ,
+      IOobject::NO_WRITE
+    ),
+    mesh
+  );
+
+  vector meanU = sum( U.internalField() ) / U.internalField().size();
+
+  Random random( 1234567 );
+  forAll( U.internalField(), iCell ){
+    U.internalField()[iCell] += (randomPercentage / 100) * meanU * random.scalar01();
+  }
+
+  U.write();
+
+  Info << nl << "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+       << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+       << nl << endl;
+
+  Info << "End\n" << endl;
+
+  return 0;
+}
+
+// ************************************************************************* //
