b9e72a60e7156dc2fa0be09ca5788835dbae8ef1
Thijs Gillebaart <thijsgillebaart@gmail.com>
2015-04-30 22:14:56 +0200

    Completed fourierTranslationBody for bodyFsi lib

--- applications/solvers/bodyFsi/moveBodyFoam/moveBodyFoam.C
+++ applications/solvers/bodyFsi/moveBodyFoam/moveBodyFoam.C
@@ -63,6 +63,8 @@ int main(int argc, char *argv[])
         ms.setMotion(bodyCol.getPatchMotion());
         mesh.update();
 
+
+		bodyCol.write();
         runTime.write();
 
         Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
