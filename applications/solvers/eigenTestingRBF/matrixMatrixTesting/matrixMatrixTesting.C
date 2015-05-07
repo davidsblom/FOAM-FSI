a05597b198269b37a54509c53640e69b5dd3e057
Thijs Gillebaart <thijsgillebaart@gmail.com>
2015-05-07 20:53:25 +0200

    Update some matrix testing

--- applications/solvers/eigenTestingRBF/matrixMatrixTesting/matrixMatrixTesting.C
+++ applications/solvers/eigenTestingRBF/matrixMatrixTesting/matrixMatrixTesting.C
@@ -51,34 +51,82 @@ Description
 
 int main(int argc, char *argv[])
 {
-    //std::shared_ptr<rbf::RBFFunctionInterface> rbfFunction = std::shared_ptr<rbf::RBFFunctionInterface>( new rbf::TPSFunction() );
-    std::shared_ptr<rbf::RBFFunctionInterface> rbfFunction = std::shared_ptr<rbf::RBFFunctionInterface>( new rbf::WendlandC2Function(1.0) );
-    rbf::inverseMatrixLibrary invLib(rbfFunction);
-
-    //unit square with Nx and Ny points
-    int Nx=30;
-    int Ny=30;
-    int N=Nx*Ny;
-    Info << "N = " << N << nl << endl;
-
-    double dx=1.0/(Nx-1.0);
-    double dy=1.0/(Ny-1.0);
-
-    Info << "Building position matrix" << endl;
-    rbf::matrix positions(N,3);
-    rbf::matrix displacement(N,1);
-    for(int i=0;i<Nx;i++){
-        for(int j=0;j<Ny;j++){
-            int k=i+j*Nx;
-            positions(k,0)=i*dx;
-            positions(k,1)=j*dy;
-            positions(k,2)=0;
-            displacement(k,0)=0.25*(1-std::cos(2*M_PI*i*dx))*(1-std::cos(2*M_PI*j*dy));
+    /*int N=100;
+    rbf::matrix C(N,N);
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            C(i,j)=rand();
         }
     }
 
+    int t_start = std::clock();
+    //const Eigen::FullPivLU<Eigen::Matrix<double, -1, -1> > Clu = Crbf.fullPivLu();
+    rbf::matrix Cinv = C.fullPivLu().inverse();
+    int t_end = std::clock();
+    double ti = 1.0*(t_end-t_start)/CLOCKS_PER_SEC;
+    Info << "Inverting C [" << N << "," << N << "]: " << ti << " s" << endl;*/
+
+    // == options == //
+    int coarsening=1;
+    int Nx=33;
+    int Ny=257;
+    double R=0.5;
+    int nTimes=50;
+
+    // == create "mesh" == //
+    //std::shared_ptr<rbf::RBFFunctionInterface> rbfFunction = std::shared_ptr<rbf::RBFFunctionInterface>( new rbf::TPSFunction() );
+    std::shared_ptr<rbf::RBFFunctionInterface> rbfFunction = std::shared_ptr<rbf::RBFFunctionInterface>( new rbf::WendlandC2Function(R) );
+    rbf::inverseMatrixLibrary invLib(rbfFunction);
+    int N=Nx*Ny;
+
+    double dx=1.0/(Nx-1);
+    double dy=1.0/(Ny-1);
+
+    Info << "Building position matrix" << endl;
+    rbf::matrix points(N,2);
+    rbf::matrix displacement(Nx,2);
+    rbf::matrix surfacePoints(Nx,2);
+    for(int i=0;i<Nx;i++){
+        for(int j=0;j<Ny;j++){
+            int k=i+j*Nx;
+            points(k,0)=i*dx;
+            points(k,1)=j*dy;
+        }
+        displacement(i,0)=std::rand();
+        displacement(i,1)=std::rand();
+        surfacePoints(i,0)=i*dx;
+        surfacePoints(i,1)=0;
+    }
+
+    int Nc=int(Nx/coarsening);
+    rbf::matrix controlPoints(Nc,2);
+    for(int i=0;i<Nc;i++){
+        controlPoints(i,0)=surfacePoints(i*coarsening,0);
+        controlPoints(i,0)=surfacePoints(i*coarsening,1);
+    }
+
+    Info << "==== mesh properties === " << endl;
+    Info << "Ni = " << N << endl;
+    Info << "Ns = " << Nx << endl;
+    Info << "Nc = " << Nc << endl;
+
+    Info<< nl << "==== Timings - Building ==== " << endl;
+    clock_t t_start = std::clock();
+    rbf::matrix Crbf(N,N);
+    invLib.getControlMatrix(points,Crbf);
+    clock_t t_end = std::clock();
+    double tcb = 1.0*(t_end-t_start)/CLOCKS_PER_SEC;
+    Info << "Building C [" << Nc << "," << Nc << "]: " << tcb << " s" << endl;
+
+    t_start = std::clock();
+    //const Eigen::FullPivLU<Eigen::Matrix<double, -1, -1> > Clu = Crbf.fullPivLu();
+    rbf::matrix Cinv = Crbf.fullPivLu().inverse();
+    t_end = std::clock();
+    double ti = 1.0*(t_end-t_start)/CLOCKS_PER_SEC;
+    Info << "Inverting C [" << Nc << "," << Nc << "]: " << ti << " s" << endl;
+
     // ========= Testing matrix calculations ==========//
-    double totalMatrixVector = 0;
+    /*double totalMatrixVector = 0;
     rbf::matrix Crbf(N,N);
     Info << "Computing rbf control matrix" << endl;
     invLib.getControlMatrix(positions,Crbf);
@@ -174,93 +222,7 @@ int main(int argc, char *argv[])
         double matrixVector = 1000.0 * (t_end-t_start)/CLOCKS_PER_SEC;
         totalMatrixVectorSymU += matrixVector;
     }
-    Info << "Symmetric Matrix (lower) vecotr product from 1x1 to " << N << "x" << N << " = " << totalMatrixVectorSymU << " ms" << endl;
-
-    /*//Row update
-    double totalUpdateInverseMS = 0;
-    rbf::matrix oldCinverse(1,1);
-    rbf::matrix newpoint(1,3);
-    oldCinverse(0,0) = 1.0;//inverse of diagonal value. For compact supported this is 1
-    for(int ip=1;ip<N;ip++){
-        //Start clocking
-        clock_t t_start = std::clock();
-
-        //Make new point
-        newpoint(0,0) = positions(ip,0);
-        newpoint(0,1) = positions(ip,1);
-        newpoint(0,2) = positions(ip,2);
-
-        //Create old positions
-        rbf::matrix oldPositions(ip,3);
-        for(int j=0;j<ip;j++){
-            oldPositions(j,0) = positions(j,0);
-            oldPositions(j,1) = positions(j,1);
-            oldPositions(j,2) = positions(j,2);
-        }
-
-        rbf::matrix inverse(ip+1,ip+1);
-        invLib.updateInverse(oldCinverse,newpoint,oldPositions,inverse);
-
-        oldCinverse = inverse;
-
-        clock_t t_end = std::clock();
-        double updateInverseMS = 1000.0 * (t_end-t_start)/CLOCKS_PER_SEC;
-        totalUpdateInverseMS += updateInverseMS;
-    }
-    Info << "Updating Inverse from 2x2 to " << N << "x" << N << " = " << totalUpdateInverseMS << " ms" << endl;
-
-    //Full inverse eigen
-    double invTimes [N-1];
-    double totalInverseEigenMS = 0;
-    Info << "Starting eigen inverse (via LU) with eigen" << endl;
-    for(int ip=1;ip<N;ip++){
-        //Start clocking
-        clock_t t_start = std::clock();
-
-        //Preparing function parameters
-        rbf::matrix curpositions(ip+1,3);
-        for(int j=0;j<ip+1;j++){
-            curpositions(j,0)=positions(ip-1,0);
-            curpositions(j,1)=positions(ip-1,1);
-            curpositions(j,2)=positions(ip-1,2);
-        }
-
-        rbf::matrix inverse(ip+1,ip+1);
-        //Computing inverse
-        invLib.computeInverseEigen(curpositions,inverse);
-
-        //Clocking
-        clock_t t_end = std::clock();
-        double inverseEigenMS = 1000.0 * (t_end-t_start)/CLOCKS_PER_SEC;
-        invTimes[ip-1]=inverseEigenMS;
-        totalInverseEigenMS += inverseEigenMS;
-    }
-    Info << "Time for Eigen inverse of 2x2 to " << N << "x" << N << " = " << totalInverseEigenMS << " ms" << endl;
-
-    //Solve system eigen
-    Info << "Starting solve system (via LU) with eigen" << endl;
-    double totalSolveEigenMS = 0;
-    for(int ip=1;ip<N;ip++){
-        //Start clocking
-        clock_t t_start = std::clock();
-
-        //Preparing function parameters
-        rbf::matrix curpositions(ip+1,3);
-        rbf::matrix curdisplacement(ip+1,1);
-        for(int j=0;j<ip+1;j++){
-            curpositions(j,0)=positions(ip-1,0);
-            curpositions(j,1)=positions(ip-1,1);
-            curpositions(j,2)=positions(ip-1,2);
-            curdisplacement(j,0)=displacement(ip-1,0);
-        }
-        rbf::matrix solution (ip+1,1);
-        invLib.solveSystemEigen(curpositions,curdisplacement,solution);
-
-        clock_t t_end = std::clock();
-        double solveEigenMS = 1000.0 * (t_end-t_start)/CLOCKS_PER_SEC;
-        totalSolveEigenMS += solveEigenMS;
-    }
-    Info << "Solving system from 2x2 to " << N << "x" << N << " = " << totalSolveEigenMS << " ms" << endl;*/
+    Info << "Symmetric Matrix (lower) vecotr product from 1x1 to " << N << "x" << N << " = " << totalMatrixVectorSymU << " ms" << endl;*/
 
     return 0;
 }
