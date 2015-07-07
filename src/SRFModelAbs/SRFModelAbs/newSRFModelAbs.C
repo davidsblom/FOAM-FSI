f544640cce4e608473beb9e216009abfad3df338
David Blom <d.s.blom@tudelft.nl>
2015-07-07 12:36:10 +0200

    Formatting

--- src/SRFModelAbs/SRFModelAbs/newSRFModelAbs.C
+++ src/SRFModelAbs/SRFModelAbs/newSRFModelAbs.C
@@ -1,29 +1,3 @@
-/*---------------------------------------------------------------------------*\
-  =========                 |
-  \\      /  F ield         | foam-extend: Open Source CFD
-   \\    /   O peration     |
-    \\  /    A nd           | For copyright notice see file Copyright
-     \\/     M anipulation  |
--------------------------------------------------------------------------------
-License
-    This file is part of foam-extend.
-
-    foam-extend is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by the
-    Free Software Foundation, either version 3 of the License, or (at your
-    option) any later version.
-
-    foam-extend is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
-
-Description
-
-\*---------------------------------------------------------------------------*/
 
 #include "SRFModelAbs.H"
 
@@ -31,61 +5,56 @@ Description
 
 namespace Foam
 {
-namespace SRF
-{
-
-// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
-
-autoPtr<SRFModelAbs> SRFModelAbs::New
-(
-    const volVectorField& U
-)
-{
-    word SRFModelAbsTypeName;
-
-    // Enclose the creation of the SRFPropertiesDict to ensure it is
-    // deleted before the SRFModelAbs is created - otherwise the dictionary
-    // is entered in the database twice
+    namespace SRF
     {
-        IOdictionary SRFPropertiesDict
-        (
-            IOobject
-            (
-                "SRFProperties",
-                U.time().constant(),
-                U.db(),
-                IOobject::MUST_READ,
-                IOobject::NO_WRITE
-            )
-        );
+        // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
-        SRFPropertiesDict.lookup("SRFModelAbs") >> SRFModelAbsTypeName;
-    }
+        autoPtr<SRFModelAbs> SRFModelAbs::New
+            ( const volVectorField & U )
+        {
+            word SRFModelAbsTypeName;
 
-    Info<< "Selecting SRFModelAbs " << SRFModelAbsTypeName << endl;
+            // Enclose the creation of the SRFPropertiesDict to ensure it is
+            // deleted before the SRFModelAbs is created - otherwise the dictionary
+            // is entered in the database twice
+            {
+                IOdictionary SRFPropertiesDict
+                (
+                    IOobject
+                    (
+                        "SRFProperties",
+                        U.time().constant(),
+                        U.db(),
+                        IOobject::MUST_READ,
+                        IOobject::NO_WRITE
+                    )
+                );
 
-    dictionaryConstructorTable::iterator cstrIter =
-        dictionaryConstructorTablePtr_->find(SRFModelAbsTypeName);
+                SRFPropertiesDict.lookup( "SRFModelAbs" ) >> SRFModelAbsTypeName;
+            }
 
-    if (cstrIter == dictionaryConstructorTablePtr_->end())
-    {
-        FatalErrorIn
-        (
-            "SRFModelAbs::New(const fvMesh&)"
-        )   << "Unknown SRFModelAbs type " << SRFModelAbsTypeName
-            << nl << nl
-            << "Valid SRFModelAbs types are :" << nl
-            << dictionaryConstructorTablePtr_->sortedToc()
-            << exit(FatalError);
-    }
+            Info << "Selecting SRFModelAbs " << SRFModelAbsTypeName << endl;
 
-    return autoPtr<SRFModelAbs>(cstrIter()(U));
-}
+            dictionaryConstructorTable::iterator cstrIter =
+                dictionaryConstructorTablePtr_->find( SRFModelAbsTypeName );
 
+            if ( cstrIter == dictionaryConstructorTablePtr_->end() )
+            {
+                FatalErrorIn
+                (
+                    "SRFModelAbs::New(const fvMesh&)"
+                ) << "Unknown SRFModelAbs type " << SRFModelAbsTypeName
+                  << nl << nl
+                  << "Valid SRFModelAbs types are :" << nl
+                  << dictionaryConstructorTablePtr_->sortedToc()
+                  << exit( FatalError );
+            }
 
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+            return autoPtr<SRFModelAbs>( cstrIter() ( U ) );
+        }
 
-} // End namespace SRF
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+    } // End namespace SRF
 } // End namespace Foam
 
 // ************************************************************************* //
