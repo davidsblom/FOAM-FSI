bdc152ea5f3918f803b93c464dad2476cb0ce01f
David Blom <d.s.blom@tudelft.nl>
2015-07-31 11:45:08 +0200

    Add test runner based on python

---
+++ src/tests/runTests.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os, subprocess, multiprocessing
+
+nbCores = multiprocessing.cpu_count()
+
+runs = []
+
+for i in range( nbCores ):
+    run = subprocess.Popen( "GTEST_TOTAL_SHARDS=" + str(nbCores) + " GTEST_SHARD_INDEX=" + str(i) + " tests > tests_" + str(i) + ".log 2>&1", shell = True )
+    runs.append( run )
+
+returnCode = 0
+for run in runs:
+    code = run.wait()
+    if code > returnCode:
+        returnCode = code
+
+for i in range( nbCores ):
+    subprocess.call("tail -n 2 tests_" + str(i) + ".log 2>&1", shell=True)
+
+exit( returnCode )
