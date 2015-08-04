07bf8889aa34e3f9d5285fb268484abee660aa32
David Blom <d.s.blom@tudelft.nl>
2015-08-04 09:42:03 +0200

    add test to verify the polynomial order of the SDC time integration schemes

--- src/tests/app/test_sdc.C
+++ src/tests/app/test_sdc.C
@@ -63,6 +63,59 @@ protected:
 
 INSTANTIATE_TEST_CASE_P( testParameters, SDCTest, ::testing::Combine( Values( 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ), Values( 1, 10, 20, 40, 80, 100, 160 ), Values( "gauss-radau", "gauss-lobatto", "clenshaw-curtis", "uniform", "uniform-right-sided" ) ) );
 
+class SDCEstimateOrderTest : public TestWithParam< std::tr1::tuple<int, std::string> >
+{
+protected:
+
+    virtual void SetUp()
+    {
+        scalar dt, q0, qdot0, As, Ac, omega, endTime, tol;
+
+        int nbNodes = std::tr1::get<0>( GetParam() );
+        int nbTimeSteps = 100;
+        std::string rule = std::tr1::get<1>( GetParam() );
+
+        endTime = 100;
+        dt = endTime / nbTimeSteps;
+        As = 100;
+        Ac = As;
+        omega = 1;
+        q0 = -As;
+        qdot0 = -As;
+        tol = 1.0e-15;
+
+        std::shared_ptr<sdc::AdaptiveTimeStepper> adaptiveTimeStepper1;
+        std::shared_ptr<sdc::AdaptiveTimeStepper> adaptiveTimeStepper2;
+
+        adaptiveTimeStepper1 = std::shared_ptr<sdc::AdaptiveTimeStepper> ( new sdc::AdaptiveTimeStepper( false ) );
+        adaptiveTimeStepper2 = std::shared_ptr<sdc::AdaptiveTimeStepper> ( new sdc::AdaptiveTimeStepper( false ) );
+
+        piston1 = std::shared_ptr<Piston> ( new Piston( nbTimeSteps, dt, q0, qdot0, As, Ac, omega ) );
+        sdc1 = std::shared_ptr<SDC> ( new SDC( piston1, adaptiveTimeStepper1, rule, nbNodes, tol ) );
+
+        nbTimeSteps *= 2;
+        dt = endTime / nbTimeSteps;
+
+        piston2 = std::shared_ptr<Piston> ( new Piston( nbTimeSteps, dt, q0, qdot0, As, Ac, omega ) );
+        sdc2 = std::shared_ptr<SDC> ( new SDC( piston2, adaptiveTimeStepper2, rule, nbNodes, tol ) );
+    }
+
+    virtual void TearDown()
+    {
+        piston1.reset();
+        piston2.reset();
+        sdc1.reset();
+        sdc2.reset();
+    }
+
+    std::shared_ptr<Piston> piston1;
+    std::shared_ptr<Piston> piston2;
+    std::shared_ptr<SDC> sdc1;
+    std::shared_ptr<SDC> sdc2;
+};
+
+INSTANTIATE_TEST_CASE_P( testParameters, SDCEstimateOrderTest, ::testing::Combine( Values( 2, 4 ), Values( "gauss-radau", "gauss-lobatto", "clenshaw-curtis", "uniform", "uniform-right-sided" ) ) );
+
 TEST_P( SDCTest, object )
 {
     ASSERT_TRUE( true );
@@ -173,6 +226,48 @@ TEST_P( SDCTest, runCompareSDC )
 
     ASSERT_NEAR( solution_piston_sdc( 0 ), solution_piston( 0 ), 1.0e-10 );
     ASSERT_NEAR( solution_piston_sdc( 1 ), solution_piston( 1 ), 1.0e-10 );
+}
+
+TEST_P( SDCEstimateOrderTest, order )
+{
+    sdc1->run();
+    sdc2->run();
+
+    fsi::vector solution1( 2 ), solution2( 2 );
+    piston1->getSolution( solution1 );
+    piston2->getSolution( solution2 );
+
+    scalar result1 = solution1( 1 );
+    scalar result2 = solution2( 1 );
+
+    scalar ref = piston1->referenceSolution( 100 );
+    scalar error1 = std::abs( result1 - ref ) / std::abs( ref );
+    scalar error2 = std::abs( result2 - ref ) / std::abs( ref );
+
+    Info << "error1 = " << error1 << ", error2 = " << error2 << endl;
+
+     int nbTimeSteps = 10;
+    scalar order = ( std::log10( error1 ) - std::log10( error2 ) ) / ( std::log10( nbTimeSteps * 2 ) - std::log10( nbTimeSteps ) );
+
+    int nbNodes = std::tr1::get<0>( GetParam() );
+    std::string rule = std::tr1::get<1>( GetParam() );
+
+    scalar tol = 3.0e-1;
+
+    if ( rule == "gauss-radau" )
+        ASSERT_NEAR( order, (nbNodes - 1) * 2 - 1, tol );
+
+    if ( rule == "gauss-lobatto" )
+        ASSERT_NEAR( order, nbNodes * 2 - 2, tol );
+
+    if ( rule == "clenshaw-curtis" )
+        ASSERT_NEAR( order, nbNodes, tol );
+
+    if ( rule == "uniform" )
+        ASSERT_NEAR( order, nbNodes, tol );
+
+    if ( rule == "uniform-right-sided" )
+        ASSERT_NEAR( order, nbNodes - 1, tol );
 }
 
 TEST( CosTest, SDC )
