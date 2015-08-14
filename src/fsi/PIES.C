8867438bcff66bfab6ff84c1fe9b7610df35f2df
David Blom <d.s.blom@tudelft.nl>
2015-08-14 10:45:01 +0200

    Use right-hand-rule for Picard Integral Exponential Solver in order to enforce a L-stable time integration scheme

--- src/fsi/PIES.C
+++ src/fsi/PIES.C
@@ -217,18 +217,23 @@ namespace sdc
         for ( int i = 0; i < t.rows(); i++ )
             t( i ) = tstart + dt * i;
 
-        A.resize( nbNodes, M );
+        A.resize( nbNodes + 1, M );
+        A.setZero();
 
-        for ( int i = 0; i < A.rows(); i++ )
-            for ( int j = 0; j < A.cols(); j++ )
+        for ( int i = 0; i < nbNodes; i++ )
+            for ( int j = 0; j < M; j++ )
                 A( i, j ) = sdc::exp( gamma_k( i ) * t( j ) );
 
-        matrixc b( A.rows(), A.cols() );
+        // Enforce right-hand-rule based nodes, which results in
+        // a L-stable time integration scheme
+        A( nbNodes, 0 ) = 0;
+
+        matrixc b( nbNodes + 1, M );
         b.setZero();
 
-        for ( int i = 0; i < b.rows(); i++ )
+        for ( int i = 0; i < nbNodes; i++ )
         {
-            for ( int j = 1; j < b.cols(); j++ )
+            for ( int j = 1; j < M; j++ )
             {
                 std::complex<longDouble> gamma = gamma_k( i );
                 longDouble t0 = t( j - 1 );
@@ -246,13 +251,15 @@ namespace sdc
         matrix smatWeights = smatReal.bottomLeftCorner( smatReal.rows() - 1, smatReal.cols() );
         smatWeights /= tinterval;
 
+        assert( (A * smatOmega - b).norm() < 1.0e-1 * delta );
+
         // Compute the qmat matrix ( t = 0 .. 1 )
 
         b.setZero();
 
-        for ( int i = 0; i < b.rows(); i++ )
+        for ( int i = 0; i < nbNodes; i++ )
         {
-            for ( int j = 1; j < b.cols(); j++ )
+            for ( int j = 1; j < M; j++ )
             {
                 std::complex<longDouble> gamma = gamma_k( i );
                 longDouble t0 = tstart;
@@ -269,6 +276,8 @@ namespace sdc
         matrix qmatReal = qmatOmega.real().transpose();
         matrix qmatWeights = qmatReal.bottomLeftCorner( qmatReal.rows() - 1, qmatReal.cols() );
         qmatWeights /= tinterval;
+
+        assert( (A * qmatOmega - b).norm() < 1.0e-1 * delta );
 
         t.array() += -tstart;
         t /= tinterval;
