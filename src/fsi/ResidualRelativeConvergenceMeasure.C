8b59359b8760dc5e7322b5072ff3dc587c61ca38
David Blom <davidsblom@gmail.com>
2015-09-04 09:16:33 +0200

    Add ResidualRelativeConvergenceMeasure class

---
+++ src/fsi/ResidualRelativeConvergenceMeasure.C
@@ -0,0 +1,65 @@
+
+/*
+ * Author
+ *   David Blom, TU Delft. All rights reserved.
+ */
+
+#include "ResidualRelativeConvergenceMeasure.H"
+
+namespace fsi
+{
+    ResidualRelativeConvergenceMeasure::ResidualRelativeConvergenceMeasure(
+        int dataId,
+        scalar convergenceLimit
+        )
+        :
+        ConvergenceMeasure( dataId ),
+        isFirstIteration( true ),
+        convergenceLimit( convergenceLimit ),
+        normFirstResidual( std::numeric_limits<double>::max() ),
+        normDiff( 0 )
+    {
+        assert( convergenceLimit > 0 );
+        assert( convergenceLimit < 1 );
+    }
+
+    void ResidualRelativeConvergenceMeasure::measure(
+        vector & oldValues,
+        vector & newValues
+        )
+    {
+        normDiff = ( newValues - oldValues ).norm();
+        if ( isFirstIteration )
+        {
+            normFirstResidual = normDiff;
+            isFirstIteration = false;
+        }
+        isConvergence_ = normDiff < normFirstResidual * convergenceLimit;
+    }
+
+    void ResidualRelativeConvergenceMeasure::newMeasurementSeries()
+    {
+        isConvergence_ = false;
+        isFirstIteration = true;
+        normFirstResidual = std::numeric_limits<double>::max();
+    }
+
+    bool ResidualRelativeConvergenceMeasure::isConvergence()
+    {
+        return isConvergence;
+    }
+
+    void ResidualRelativeConvergenceMeasure::printState()
+    {
+        Info << "residual relative convergence measure: ";
+        Info << "two-norm diff = " << normDiff;
+        Info << ", limit = " << normFirstResidual * convergenceLimit;
+        Info << ", conv = ";
+        if ( isConvergence )
+            Info << "true";
+        else
+            Info << "false";
+        Info << endl;
+    }
+
+}
