f987e9bb6dc37f75157f22a15500ea14ced46d9b
David Blom <davidsblom@gmail.com>
2016-01-19 17:34:15 +0100

    Fix SDC test

--- src/tests/app/test_sdc.C
+++ src/tests/app/test_sdc.C
@@ -225,7 +225,7 @@ TEST_P( SDCTest, runCompareSDC )
     piston_sdc->getSolution( solution_piston_sdc, f );
 
     ASSERT_NEAR( solution_piston_sdc( 0 ), solution_piston( 0 ), 1.0e-8 );
-    ASSERT_NEAR( solution_piston_sdc( 1 ), solution_piston( 1 ), 1.0e-8 );
+    ASSERT_NEAR( solution_piston_sdc( 1 ), solution_piston( 1 ), 1.0e-7 );
 }
 
 TEST_P( SDCEstimateOrderTest, order )
