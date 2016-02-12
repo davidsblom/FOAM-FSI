839f0ffa54b2a7190c213b3a232626dc352acc98
David Blom <davidsblom@gmail.com>
2016-02-12 18:14:54 +0100

    test suite: use separate executables

--- src/tests/runTests.py
+++ src/tests/runTests.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python
 
-import os, subprocess, multiprocessing, time, sys
+import os, subprocess, multiprocessing, time, sys, argparse
 
-nbCores = multiprocessing.cpu_count()
+nbCores = int( os.environ['WM_NCOMPPROCS'] )
+
+parser = argparse.ArgumentParser( description='Run the test suite' )
+parser.add_argument('testsuite', help='which testsuite: testsuite-dealii, testsuite-rbf, testsuite-spacemapping, testsuite-fsi, or testsuite-sdc' )
+args = parser.parse_args()
 
 runs = []
 
 for i in range( nbCores ):
-    run = subprocess.Popen( "GTEST_TOTAL_SHARDS=" + str(nbCores) + " GTEST_SHARD_INDEX=" + str(i) + " tests --gtest_throw_on_failure > tests_" + str(i) + ".log 2>&1", shell = True )
+    run = subprocess.Popen( "GTEST_TOTAL_SHARDS=" + str(nbCores) + " GTEST_SHARD_INDEX=" + str(i) + " " + args.testsuite + " --gtest_throw_on_failure > tests_" + str(i) + ".log 2>&1", shell = True )
     runs.append( run )
 
 returnCode = 0
 for run in runs:
     code = None
     while code is None:
-        time.sleep( 5 )
         print '.'
         sys.stdout.flush()
         code = run.poll()
         if code > returnCode:
             returnCode = code
+        if code is not None:
+            break
+        time.sleep( 5 )
 
 for i in range( nbCores ):
-    subprocess.call("tail -n 50 tests_" + str(i) + ".log 2>&1", shell=True)
+    subprocess.call("tail -n 20 tests_" + str(i) + ".log 2>&1", shell=True)
 
 if returnCode == 0:
     print "Finished successfully"
