99f9bb08277303f107c43f13c4ea1b5e0de421d0
David Blom <davidsblom@gmail.com>
2016-02-12 15:47:32 +0100

    third party compilation: always use $WM_NCOMPPROCS for every dependency

--- src/tests/runTests.py
+++ src/tests/runTests.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-import os, subprocess, multiprocessing, time
+import os, subprocess, multiprocessing, time, sys
 
 nbCores = multiprocessing.cpu_count()
 
 runs = []
 
 for i in range( nbCores ):
-    run = subprocess.Popen( "GTEST_TOTAL_SHARDS=" + str(nbCores) + " GTEST_SHARD_INDEX=" + str(i) + " tests > tests_" + str(i) + ".log 2>&1", shell = True )
+    run = subprocess.Popen( "GTEST_TOTAL_SHARDS=" + str(nbCores) + " GTEST_SHARD_INDEX=" + str(i) + " tests --gtest_throw_on_failure > tests_" + str(i) + ".log 2>&1", shell = True )
     runs.append( run )
 
 returnCode = 0
@@ -16,12 +16,16 @@ for run in runs:
     while code is None:
         time.sleep( 5 )
         print '.'
+        sys.stdout.flush()
         code = run.poll()
         if code > returnCode:
             returnCode = code
-    print code
 
 for i in range( nbCores ):
     subprocess.call("tail -n 50 tests_" + str(i) + ".log 2>&1", shell=True)
 
+if returnCode == 0:
+    print "Finished successfully"
+else:
+    print "Tests failed"
 exit( returnCode )
