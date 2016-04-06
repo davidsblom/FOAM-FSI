619c52da00a4aa5f8add0e37398d44a28a801c70
David Blom <davidsblom@gmail.com>
2016-04-06 17:59:10 +0200

    Fix unit tests

--- src/tests/testsuite-sdc-fsi/test_sdctubeflowfluidsolver.C
+++ src/tests/testsuite-sdc-fsi/test_sdctubeflowfluidsolver.C
@@ -30,16 +30,14 @@ class SDCFluidSolverTest : public ::testing::Test
             scalar cmk = std::sqrt( E * h / (2 * rho * r0) );
             scalar c0 = std::sqrt( cmk * cmk - p0 / (2 * rho) );
             scalar kappa = c0 / u0;
-            scalar tau = u0 * dt / L;
 
-            ASSERT_NEAR( tau, 0.01, 1.0e-13 );
             ASSERT_NEAR( kappa, 10, 1.0e-13 );
             ASSERT_TRUE( dx > 0 );
 
             std::shared_ptr<tubeflow::SDCTubeFlowFluidSolver> fluid( new tubeflow::SDCTubeFlowFluidSolver( a0, u0, p0, dt, cmk, N, L, T, rho ) );
 
-            int nbNodes = 6;
-            scalar tol = 1.0e-11;
+            int nbNodes = 1;
+            scalar tol = 1.0e-14;
 
             std::shared_ptr<fsi::quadrature::IQuadrature<scalar> > quadrature;
             quadrature = std::shared_ptr<fsi::quadrature::IQuadrature<scalar> >( new fsi::quadrature::GaussRadau<scalar>( nbNodes ) );
