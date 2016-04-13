b1bc0282665851f54578a88ff03329c0dae34753
David Blom <davidsblom@gmail.com>
2016-04-13 17:44:12 +0200

    tube flow: explicit solid solver uses the 3/8 rule for RK4

--- src/tests/tubeflow-sdc/tubeflow-sdc.C
+++ src/tests/tubeflow-sdc/tubeflow-sdc.C
@@ -29,7 +29,7 @@ int main()
         "IDC", "SDIRK"
     };
     std::vector<std::string> sdirkSchemes = {
-        "SDIRK2", "SDIRK3", "SDIRK4"
+        "SDIRK2", "SDIRK3", "SDIRK4", "ESDIRK74PR"
     };
 
     for ( auto timeIntegrationSchemeString : timeIntegrationSchemes )
